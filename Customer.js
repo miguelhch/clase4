823eb38e5a43ce9a28d84da4ec1c5861abc1ae24
Miguel Higorre <miguel.higorre@gmail.com>
2025-05-27 11:01:17 -0400

    Agregar gestion de clientes

---
+++ Customer.js
@@ -0,0 +1,3 @@
+console.log("Menu de clientes"); // Este es un comentario de una sola línea
+console.log("1. Agregar cliente"); // Este es un comentario de una sola línea
+console.log("2. Eliminar cliente"); // Este es un comentario de una sola línea
